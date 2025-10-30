d1c9731b2080ecf9f7b082e9679a5318225f9e7e
Gabriel Loureiro <157625824+GabrielLoureiro09@users.noreply.github.com>
2025-10-29 22:36:10 -0300

    feat/classe-condena: Arrumando nome do campo na classe condena

--- src/main/java/org/igesta/dto/CondenaRequestDTO.java
+++ src/main/java/org/igesta/dto/CondenaRequestDTO.java
@@ -14,7 +14,7 @@ import org.igesta.validation.OnCreate;
 public class CondenaRequestDTO {
 
     @Schema(description = "Identificador único da condena", example = "1")
-    private Integer id;
+    private Integer idCondena;
 
     @Schema(description = "Nome da condena", example = "Aero Saculite T")
     @NotNull(groups = OnCreate.class)
